d296cb93fdb8d43bc7519fd05f607bc183741a20
junhyuk <dejavukk1@gmail.com>
2021-12-17 16:47:46 +0900

    UIColor+TC.swift 파일 추가(스타터팩)

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
 
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
